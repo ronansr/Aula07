947a870ff40ea8a47fc8a917d0ae3ce58e5ff871
ice <ice@lab06m08.nrc.ice.ufjf.br>
2018-08-27 20:13:02 -0300

    Revisao de envio, qtd de instancias de um obj

--- Aula7/src/aula7/Aula7.java
+++ Aula7/src/aula7/Aula7.java
@@ -7,5 +7,10 @@ package aula7;
 public class Aula7 {
     public static void main(String[] args) {
     }
+    /*
+    como saber quantas instancias exitem de um objeto?
     
+    colocando dentro da classe uma variavel estatica, onde sera incrementada a cada new,
+    e decrementada a cada delete
+    */
 }
