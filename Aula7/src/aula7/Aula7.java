774d52fde4a14bff6f2f84529775c5f027349f78
ice <ice@lab06m08.nrc.ice.ufjf.br>
2018-08-27 20:37:08 -0300

    Revisao de envio, qtd de instancias de um obj

--- Aula7/src/aula7/Aula7.java
+++ Aula7/src/aula7/Aula7.java
@@ -12,5 +12,19 @@ public class Aula7 {
     
     colocando dentro da classe uma variavel estatica, onde sera incrementada a cada new,
     e decrementada a cada delete
+    deve ser uma val estatica pq a cada instancia criada a val qtd é inicializada novamente
+    o atributo/metodo estatica e comum a todas as instancias, por pertencer aa classe
+    
+    public static int qtdCachorro = 0;
+    #observar o debug
+    ----
+    //maneiras de remover uma instancia cachorro
+    void remove(Cachorro c){
+        c = null;
+    }
+    Cachorro remove(){
+        return null;
+    }
     */
+    
 }
