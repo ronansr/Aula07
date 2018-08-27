1a8dbccd6cafad0ab72d43f8c0e4d2cef1e27e51
ice <ice@lab06m08.nrc.ice.ufjf.br>
2018-08-27 20:08:40 -0300

    Revisao de envio

--- Aula7/src/aula7/Aula7.java
+++ Aula7/src/aula7/Aula7.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// enviando para o github
+//  - clonar
+//  - equipe/adicionar
+//  - criar commit
+//  - remoto/expandir
 package aula7;
-
-/**
- *
- * @author ice
- */
 public class Aula7 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
